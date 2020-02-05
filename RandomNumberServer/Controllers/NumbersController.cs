b4ddc7349eb8e2a8d021f705ac46e4d07f9c396b
VL <V.lastauskas@protonmail.com>
2020-02-05 23:07:04 +0200

    init

---
+++ RandomNumberServer/Controllers/NumbersController.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using RandomNumberServer.Hubs;
+
+namespace RandomNumberServer.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class NumbersController : ControllerBase
+    {
+        private readonly IHubContext<NumbersBroadcastHub> _context;
+
+        public NumbersController(IHubContext<NumbersBroadcastHub> context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("{senderId}/{number}")]
+        public Task PublishNumber(int senderId, int number)
+        {
+            return _context.Clients.All.SendAsync("PublishNumber", senderId, number);
+        }
+    }
+}
---
+++ RandomNumberServer/Hubs/NumbersBroadcastHub.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace RandomNumberServer.Hubs
+{
+    public class NumbersBroadcastHub : Hub
+    {
+        public const string PublishEventName = "PublishNumber";
+
+        public Task BroadcastNumber(int senderId, int number)
+        {
+            return Clients.All.SendAsync(PublishEventName, senderId, number);
+        }
+    }
+}
